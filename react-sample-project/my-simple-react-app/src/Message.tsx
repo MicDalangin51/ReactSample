55810ef4a41a25f385b753470344c2e015e593e9
MicDalangin51 <martindalangin@gmail.com>
2024-07-06 11:30:39 +0800

    build: initialize project environment

---
+++ react-sample-project/my-simple-react-app/src/Message.tsx
@@ -0,0 +1,8 @@
+
+// PascalCasing means capitalizing the first letter of every word
+function Message() {
+    const message_text = 'My App'
+    return <h1>Welcome to {message_text}!</h1>;
+}
+
+export default Message;
